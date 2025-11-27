c1e7614ad3d9a9be4f8579bb20a27ae753a22adb
Ian Rose Aaron Mendoza <ianrose.aaronmendoza@gmail.com>
2025-11-27 11:00:23 +0800

    Full app scaffold

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,8 +1,8 @@
 export const msalConfig = {
   auth: {
     clientId: "f7d7ec84-9d43-47a2-85e1-0129539c41e4",
-    authority: "https://login.microsoftonline.com/a035ba86-23cc-4705-b45e-c98228a6eeb5",
-    redirectUri: "http://localhost:5173", 
+    authority: "https://login.microsoftonline.com/a035ba86-23cc-4705-b45e-c98228a6eeb5", 
+    redirectUri: "https://smart-receipt-logger.vercel.app",
   }
 };
 
