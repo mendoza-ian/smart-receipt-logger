1352d28d2586885bf4b5d1e6101f9ddcfcfbec73
Ian Rose Aaron Mendoza <ianrose.aaronmendoza@gmail.com>
2025-11-27 11:14:27 +0800

    Switch to corporate Azure SSO app

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,8 +1,8 @@
 export const msalConfig = {
   auth: {
-    clientId: "f7d7ec84-9d43-47a2-85e1-0129539c41e4",
-    authority: "https://login.microsoftonline.com/a035ba86-23cc-4705-b45e-c98228a6eeb5", 
-    redirectUri: "https://smart-receipt-logger.vercel.app",
+    clientId: "f62943ed-0c4a-4aa7-b047-79a610fc32a1",
+    authority: "https://login.microsoftonline.com/a035ba86-23cc-4705-b45e-c98228a6eeb5",
+    redirectUri: "https://smart-receipt-logger.vercel.app",  // IMPORTANT
   }
 };
 
