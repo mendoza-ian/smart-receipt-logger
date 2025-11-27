8aead168a4758c2aeeb68a6a0007cfa42bfa6da1
Ian Rose Aaron Mendoza <ianrose.aaronmendoza@gmail.com>
2025-11-27 11:51:03 +0800

    Fix MSAL client ID to IBS corporate app

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "f62943ed-0c4a-4aa7-b047-79a610fc32a1",
+    clientId: "f628493d-dc34-4aa7-bc47-79a1640fc3a1",
     authority: "https://login.microsoftonline.com/a035ba86-23cc-4705-b45e-c98228a6eeb5",
     redirectUri: "https://smart-receipt-logger.vercel.app",  // IMPORTANT
   }
